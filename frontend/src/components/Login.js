89c16ed94bc224781ae922cdbf750189f4a41c72
Quonnaq0711 <shaquonna.lynna@gmail.com>
2025-08-21 22:56:38 -0400

    reg debug

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -43,12 +43,7 @@ const Login = () => {
     }
   };
 
-  const handlePassword = async (e) => {
-    e.preventDefault();
-    navigate('/rest_password_request')
-  }
-
-  return (
+   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
